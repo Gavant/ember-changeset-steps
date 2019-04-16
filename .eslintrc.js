db4c1aa31d32a3527f6f1abe05259fb743b7a632
Adam Baker <bakerac4@gmail.com>
2019-04-16 10:22:01 -0400

    Fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,46 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: "module"
-  },
-  plugins: ["ember", "prettier"],
-  extends: ["eslint:recommended", "plugin:ember/recommended"],
-  env: {
-    browser: true
-  },
-  rules: {},
-  overrides: [
-    // node files
-    {
-      files: [
-        ".eslintrc.js",
-        ".template-lintrc.js",
-        "ember-cli-build.js",
-        "index.js",
-        "testem.js",
-        "blueprints/*/index.js",
-        "config/**/*.js",
-        "tests/dummy/config/**/*.js"
-      ],
-      excludedFiles: [
-        "addon/**",
-        "addon-test-support/**",
-        "app/**",
-        "tests/dummy/app/**"
-      ],
-      parserOptions: {
-        sourceType: "script",
-        ecmaVersion: 2015
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ["node"],
-      rules: Object.assign(
-        {},
-        require("eslint-plugin-node").configs.recommended.rules,
+    root: true,
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module'
+    },
+    plugins: ['ember', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:ember/recommended'],
+    env: {
+        browser: true
+    },
+    rules: {},
+    overrides: [
+        // node files
         {
-          // add your custom rules and overrides for node files here
+            files: [
+                '.eslintrc.js',
+                '.template-lintrc.js',
+                'ember-cli-build.js',
+                'index.js',
+                'testem.js',
+                'blueprints/*/index.js',
+                'config/**/*.js',
+                'tests/dummy/config/**/*.js'
+            ],
+            excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
+            parserOptions: {
+                sourceType: 'script',
+                ecmaVersion: 2015
+            },
+            env: {
+                browser: false,
+                node: true
+            },
+            plugins: ['node'],
+            rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+                'node/no-unpublished-require': [
+                    'error',
+                    {
+                        allowModules: ['ember-cli-changelog']
+                    }
+                ]
+            })
         }
-      )
-    }
-  ]
+    ]
 };
